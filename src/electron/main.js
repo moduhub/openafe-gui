91179007baa65db4f0b69fbdadfe38626a8c6e97
Adan Kvitschal <adan@moduhub.com>
2023-08-17 13:46:52 -0300

    [feat] added auto authorization to serial ports

--- src/electron/main.js
+++ src/electron/main.js
@@ -6,10 +6,50 @@ function createWindow () {
     show: false,
     titleBarStyle: 'customButtonsOnHover'
   });
-    win.maximize();
-    win.show();
-    win.loadURL('http://localhost:3000')
-  }
+
+  win.webContents.session.on('select-serial-port', (event, portList, webContents, callback) => {
+    // Add listeners to handle ports being added or removed before the callback for `select-serial-port`
+    // is called.
+    console.log('select-serial-port FIRED WITH', portList);
+    win.webContents.session.on('serial-port-added', (event, port) => {
+      console.log('serial-port-added FIRED WITH', port)
+      // Optionally update portList to add the new port
+    })
+
+    win.webContents.session.on('serial-port-removed', (event, port) => {
+      console.log('serial-port-removed FIRED WITH', port)
+      // Optionally update portList to remove the port
+    })
+
+    event.preventDefault()
+    if (portList && portList.length > 0) {
+      callback(portList[0].portId)
+    } else {
+      // eslint-disable-next-line n/no-callback-literal
+      callback('') // Could not find any matching devices
+    }
+  })
+
+  win.webContents.session.setPermissionCheckHandler((webContents, permission, requestingOrigin, details) => {
+    //if (permission === 'serial' && details.securityOrigin === 'file:///') { //this is more conservative
+    if (permission === 'serial') {
+      return true
+    }
+    return false
+  })
+
+  win.webContents.session.setDevicePermissionHandler((details) => {
+    //if (details.deviceType === 'serial' && details.origin === 'file://') { //this is more conservative
+    if (details.deviceType === 'serial') {
+      return true
+    }
+    return false
+  })
+
+  win.maximize();
+  win.show();
+  win.loadURL('http://localhost:3000')
+}
 
  
 
