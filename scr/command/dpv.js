2a185d745b6f79f2ee14dc0e2e050bfa6963318c
willianm18 <willian_ramthun@hotmail.com>
2023-10-21 17:59:22 -0300

    DPV inclued

---
+++ scr/command/dpv.js
@@ -0,0 +1,25 @@
+const dpvForm = document.getElementById("dpvForm");
+
+    dpvForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    var setTime = document.getElementById("setTime");
+    var startP = document.getElementById("startP");
+    var endP = document.getElementById("endP")
+    var pulseP = document.getElementById("pulseP")
+    var step = document.getElementById("step")
+    var pulseW = document.getElementById("pulseW")
+    var baseW = document.getElementById("baseW")
+    var periodP = document.getElementById("periodP")
+    var periodB = document.getElementById("periodB")
+
+
+    const commandInput = "DPV," + setTime.value + "," + startP.value + "," + endP.value + ","+ pulseP.value + ","+ step.value + ","+ pulseW.value + ","+ baseW.value + ","+ periodP.value + ","+ periodB.value
+    
+    const checksum = await calculateChecksum(commandInput);
+    console.log(commandInput)
+    /* const commandInput = 1 */
+    const fullCommandInput = "$" + commandInput + "*" + checksum
+    console.log(fullCommandInput);
+    sendData(fullCommandInput);
+  });
