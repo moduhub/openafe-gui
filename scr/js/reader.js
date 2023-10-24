5a674ff93703cbf08e74747784ee8d18ba3091c5
willianm18 <willian_ramthun@hotmail.com>
2023-10-24 08:43:12 -0300

    handling commands

--- scr/js/reader.js
+++ scr/js/reader.js
@@ -1,5 +1,6 @@
 let receivedData = '';
 let reader = null;
+let sglLineCount = 0; // Inicialize o contador
 
 async function startReading() {
     if (reader) {
@@ -26,41 +27,60 @@ async function startReading() {
                 receivedData = lines.pop();
     
                 for (const line of lines) {
+                    if (line.startsWith("$MSG,END*3A")) {
+                        reader.releaseLock();
+                        reader = null;
+                        console.log('Parando a leitura.');
+                        console.log(sglLineCount)
+                        sglLineCount = 0
+                    }
+    
                     if (line.startsWith("$SGL,")) {
+                        sglLineCount++; // Incrementa o contador de linhas $SGL:
                         const part = line.split(',');
                         if (part.length >= 3) {
                             const x = parseFloat(part[1]);
                             const y = parseFloat(part[2]);
                             console.log('SGL:' + x);
                             console.log('y:' + y);
-
-                            
-                            const data = line.split("*")
-                            const dataChecksum = data[0].replace("$", "").trim();
-                            const checksum = await calculateChecksum(dataChecksum);
-                            const z = String(data[1].split(",\r")[0]).trim(); 
-                            console.log("Checksum:" + checksum + "'")
-                            console.log("Ultimo:" + z + "'")
-                            console.log("Checksum:" + (checksum === z))
-
     
-                            addDataToChart(x, y);
+                            const data = line.split("*");
+                            if (data.length === 2) {
+                                const dataChecksum = data[0].replace("$", "").trim();
+                                const checksum = await calculateChecksum(dataChecksum);
+                                const z = String(data[1].split(",\r")[0]).trim(); 
+                                console.log("Checksum:" + checksum + "'");
+                                console.log("Ultimo:" + z + "'");
+                                console.log("Checksum:" + (checksum === z))
+    
+                                addDataToChart(x, y);
+                            } else {
+                                // Registro de erro
+                                console.log('Dados com problema: linha com formato incorreto -', line);
+                            }
+                        } else {
+                            // Registro de erro
+                            console.log('Dados com problema: linha com número insuficiente de partes -', line);
                         }
+                    } else {
+                        // Registro de erro
+                        console.log('Dados com problema: linha não começa com "$SGL," -', line);
                     }
                 }
+
             }
         }
     } catch (error) {
         console.error('Erro na leitura:', error);
     }
-}
+}     
 
 // Function to stop reading when necessary
 function stopReading() {
     if (reader) {
+        sendData("$CMD,DIE*2E")
         reader.releaseLock();
         reader = null;
-        sendData("DIE")
     }
 }
 
