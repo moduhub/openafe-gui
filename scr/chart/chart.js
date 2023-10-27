57c730f10c50a9dbb4c6037d7daf4ce5a0697a16
willianm18 <willian_ramthun@hotmail.com>
2023-10-26 22:54:47 -0300

    Chart setting update

--- scr/chart/chart.js
+++ scr/chart/chart.js
@@ -2,6 +2,10 @@ const title = document.getElementById('title');
 let dynamicTitle = ' ';
 let mychart;
 
+const binIconSVG =  {'width': 500,
+'height': 600,
+'path': 'M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z'
+}
 const chartData = [{
     x: [],
     y: [],
@@ -30,10 +34,11 @@ const layout = {
         mirror: true
     },
     modebar: {
-        // vertical modebar button layout
         orientation: 'v',
-        // for demonstration purposes
-      },
+        bgcolor: 'rgba(255 ,255 ,255 ,0.7)',
+        iconColor: 'red',
+        logoColor: 'rgba(0, 31, 95, 0.3)',
+    },
 };
 
 const config = {
@@ -41,6 +46,17 @@ const config = {
     displaylogo: false,
     displayModeBar: true,
     responsive: true,
+    modeBarButtonsToAdd: [
+        {
+          name: 'Clear Chart',
+          icon: binIconSVG,
+          direction: 'up',
+          click: function() {    
+            chartData[0].x = [];
+            chartData[0].y = [];
+            updateData();
+          }}],
+
 }
 
 // Função para renderizar o gráfico Plotly
@@ -49,12 +65,6 @@ const render = function() {
 };
 render();
 
-// Limpar o gráfico
-document.getElementById('btn-clear').onclick = function() {
-    chartData[0].x = [];
-    chartData[0].y = [];
-    updateData();
-};
 
 title.addEventListener('input', (e) => {
     dynamicTitle = e.target.value;
@@ -78,14 +88,4 @@ function addDataToChart(x, y) {
     chartData[0].y.push(y);
     updateData();
 }
-
-document.getElementById('btn-download').onclick = function() {
-    // Baixar o gráfico como imagem
-    Plotly.toImage('mychart', { format: 'png' })
-        .then(function(url) {
-            var a = document.createElement('a');
-            a.href = url;
-            a.download = 'my_file_name.png';
-            a.click();
-        });
-};
+    
