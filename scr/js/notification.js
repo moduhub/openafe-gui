41a3d331a25f6ca8d72424e055dba9c25aaa48f2
willianm18 <willian_ramthun@hotmail.com>
2023-10-30 09:06:04 -0300

    Improved notification

--- scr/js/notification.js
+++ scr/js/notification.js
@@ -1,37 +1,24 @@
-const connectedDevice = document.getElementById('connectedDevice');
-  const disconnectedDevice = document.getElementById('disconnectedDevice');
-  const closeNotificationButtons = document.querySelectorAll('.closeNotification');
-  let notificationTimeout;
   const notificationDelay = 2000; // Tempo de atraso em milissegundos (2 segundos)
+  var notificationTimeout;
+  
+  function openNotification(notificationID) {
+    const notification = document.getElementById(notificationID);
+    notification.classList.remove('hidden');
+    notificationTimeout = setTimeout(() => {closeNotification(notification)}, notificationDelay);
+}
 
-  function notifyConnected() {
-    connectedDevice.classList.remove('hidden');
-    
-    notificationTimeout = setTimeout(closeConnectedNotification, notificationDelay);
-  }
+function openNotificationNotClose(notificationID) {
+  const notification = document.getElementById(notificationID);
+  notification.classList.remove('hidden');
+}
 
-  function notifyDisconnected() {
-    disconnectedDevice.classList.remove('hidden');
-
-    notificationTimeout = setTimeout(closeDisconnectedNotification, notificationDelay);
-  }
-
-  function closeConnectedNotification() {
-    connectedDevice.classList.add('hidden');
+  function closeNotification(notification) {
+    notification.classList.add('hidden');
     clearTimeout(notificationTimeout);
   }
 
-  function closeDisconnectedNotification() {
-    disconnectedDevice.classList.add('hidden');
-    clearTimeout(notificationTimeout);
-  }
-
-  closeNotificationButtons.forEach(button => {
-    button.addEventListener('click', notifyClose);
-  });
-
-  function notifyClose() {
-    connectedDevice.classList.add('hidden');
-    disconnectedDevice.classList.add('hidden');
-    clearTimeout(notificationTimeout);
-  }
+  function handleClickCloseButton(button) {
+    const notificationID = button.getAttribute('data-notification-id')
+    const notificantion = document.getElementById(notificationID)
+    notificantion.classList.add('hidden')
+}
